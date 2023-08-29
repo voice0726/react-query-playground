9bd47763705aa6eeee660dd1ebc2de985afb6155
Akinori Ito <voice0726@gmail.com>
2023-08-29 14:19:51 +0900

    feat: add eslint plugin for react-query

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,102 +1,110 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2022": true
+  env: {
+    browser: true,
+    es2022: true,
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:react/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:react/jsx-runtime",
-    "next/core-web-vitals",
-    "prettier"
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/jsx-runtime',
+    'next/core-web-vitals',
+    'prettier',
   ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "tsconfigRootDir": ".",
-    "project": ["./tsconfig.json"],
-    "sourceType": "module"
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    project: './tsconfig.json',
+    sourceType: 'module',
   },
-  "plugins": [
-    "@typescript-eslint",
-    "jsx-a11y",
-    "react"
+  plugins: [
+    '@typescript-eslint',
+    '@tanstack/eslint-plugin-query',
+    'jsx-a11y',
+    'react',
   ],
-  "rules": {
-    "@typescript-eslint/consistent-type-definitions": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-misused-promises": [
-      "error",
-      { 'checksVoidReturn': false }
+  rules: {
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      { checksVoidReturn: false },
     ],
-    "@typescript-eslint/no-unused-vars": [
-      "error",
+    '@typescript-eslint/no-unused-vars': [
+      'error',
       {
-        "argsIgnorePattern": "^_",
-        "varsIgnorePattern": "^_"
-      }
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
     ],
-    "@typescript-eslint/triple-slash-reference": [
-      "error",
+    '@typescript-eslint/triple-slash-reference': [
+      'error',
       {
-        "types": "always",
-      }
+        types: 'always',
+      },
     ],
-    "import/extensions": [
-      "error",
+    'import/extensions': [
+      'error',
       {
-        "ignorePackages": true,
-        "pattern": {
-          "js": "never", "jsx": "never", "ts": "never", "tsx": "never"
-        }
-      }
-    ],
-    "import/order": [
-      "error",
-      {
-        "groups": [
-          "builtin", "external", "internal", "parent", "sibling", "index"
-        ],
-        "newlines-between": "always",
-        "pathGroupsExcludedImportTypes": ["builtin"],
-        "alphabetize": {
-          "order": "asc",
+        ignorePackages: true,
+        pattern: {
+          js: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never',
         },
-        "pathGroups": [
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        'newlines-between': 'always',
+        pathGroupsExcludedImportTypes: ['builtin'],
+        alphabetize: {
+          order: 'asc',
+        },
+        pathGroups: [
           {
-            "pattern": "{react,react-dom/**}",
-            "group": "builtin",
-            "position": "before"
+            pattern: '{react,react-dom/**}',
+            group: 'builtin',
+            position: 'before',
           },
           {
-            "pattern": "{[A-Z]*,**/[A-Z]*}",
-            "group": "internal",
-            "position": "after"
+            pattern: '{[A-Z]*,**/[A-Z]*}',
+            group: 'internal',
+            position: 'after',
           },
           {
-            "pattern": "./**.module.css",
-            "group": "index",
-            "position": "after"
-          }
-        ]
-      }
+            pattern: './**.module.css',
+            group: 'index',
+            position: 'after',
+          },
+        ],
+      },
     ],
   },
-  "overrides": [
+  overrides: [
     {
-      "files": ["*.tsx"],
-      "rules": {
-        "react/prop-types": "off",
-      }
-    }
+      files: ['*.tsx'],
+      rules: {
+        'react/prop-types': 'off',
+      },
+    },
   ],
-  "settings": {
-    "react": {
-      "version": "detect"
-    }
-  }
-}
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
